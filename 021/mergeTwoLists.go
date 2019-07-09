33a77c97309d55d11402fbb4c1383568de089d7b
jerry <jerrystarry@gmail.com>
2019-07-09 16:17:19 +0800

    两个有序链表合并为一个新的有序链表

---
+++ 021/mergeTwoLists.go
@@ -0,0 +1,27 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+// 将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+
+	if l1.Val < l2.Val {
+		l1.Next = mergeTwoLists(l1.Next, l2)
+		return l1
+	}
+	l2.Next = mergeTwoLists(l1, l2.Next)
+	return l2
+}
+
+func main() {
+
+}

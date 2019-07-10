8c09945701447d107a89fd4351c2f6c52dc8e5dd
jerry <jerrystarry@gmail.com>
2019-07-10 13:42:58 +0800

    编写一个函数求n的阶乘

---
+++ recursive/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+// 编写一个函数求n的阶乘
+// 在数学中，正整数的阶乘（英语：factorial）是所有小于及等于该数的正整数的积，计为n!，例如5的阶乘计为5!，其值为120：
+// 并定义，1的阶乘1!为1、0的阶乘0!亦为1，其中，0的阶乘表示一个空积[2]。
+func factorial(n int64) int64 {
+	if n == 0 || n == 1 {
+		return 1
+	}
+	return n * factorial(n-1)
+}
+
+func main() {
+	fmt.Print("Enter a number: ")
+	var n int64
+	fmt.Scanln(&n)
+	f := factorial(n)
+	fmt.Printf("%d的阶乘是%d\n", n, f)
+}

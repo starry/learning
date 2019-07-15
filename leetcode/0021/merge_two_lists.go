da4e44c98b5a666bfa2d164053c5089453d59f6c
Jerry <jerrystarry@gmail.com>
2019-07-15 17:47:31 +0800

    将两个有序链表合并为一个新的有序链表并返回

---
+++ leetcode/0021/merge_two_lists.go
@@ -0,0 +1,20 @@
+package leetcode
+
+import "learning/util"
+
+// 将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+func mergeTwoLists(l1 *util.ListNode, l2 *util.ListNode) *util.ListNode {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+
+	if l1.Val < l2.Val {
+		l1.Next = mergeTwoLists(l1.Next, l2)
+		return l1
+	}
+	l2.Next = mergeTwoLists(l1, l2.Next)
+	return l2
+}
